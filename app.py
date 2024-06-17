d7ef8d6697580a5207404e99ae10f9420638686b
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2024-06-17 08:24:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ origins = ["http://localhost:4000"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

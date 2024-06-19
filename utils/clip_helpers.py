2be0f25cd764c833603758843da2625d249377d3
MiuruHan <abeysinghechanuka@gmail.com>
2024-06-19 16:02:14 +0530

    U

--- utils/clip_helpers.py
+++ utils/clip_helpers.py
@@ -2,12 +2,10 @@ from functools import partial
 from torchmetrics.functional.multimodal import clip_score
 import torch
 
-clip_score_fn = partial(
-    clip_score, model_name_or_path="openai/clip-vit-base-patch16")
+clip_score_fn = partial(clip_score, model_name_or_path="openai/clip-vit-base-patch16")
 
 
 def get_clip_score(images, prompts):
     images_int = (images * 255).astype("uint8")
-    clip_score = clip_score_fn(torch.from_numpy(
-        images_int).permute(0, 3, 1, 2), prompts).detach()
-    return round(float(clip_score), 4)
+    clip_score_val = clip_score_fn(torch.from_numpy(images_int).permute(0, 3, 1, 2), prompts).detach()
+    return round(float(clip_score_val), 4)
